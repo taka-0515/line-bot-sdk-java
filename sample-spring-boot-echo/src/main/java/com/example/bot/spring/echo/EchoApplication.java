f41f685590cbf81517a41dbce05bdfecf946ef5a
yoshiyuki <yoshiyuki@yoshiyuki-System-Product-Name>
2021-11-11 01:11:30 +0900

    hotpepper追加

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,11 +16,21 @@
 
 package com.example.bot.spring.echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -42,12 +52,69 @@ public class EchoApplication {
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         log.info("event: " + event);
         final String originalMessageText = event.getMessage().getText();
-        final String subText = "それロマサガの話？";
+        
+
+        	String area = "&lage_area=Z063";
+        	String keyword = "&keyword="+originalMessageText;
+        
+        
+        final String subText = shopAndKuchikomiSearch(area+keyword);
         return new TextMessage(subText);
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
+    }
+    
+    // 試しにリクエスト用のメソッド
+    public String shopAndKuchikomiSearch(String str) {
+
+    	// クライアントから受け取ったパラメータをキーに、Hotpepper APIから店舗情報を取得する。(変数とかは自分で設定）
+    	String url = "http://webservice.recruit.co.jp/hotpepper/shop/v1/?key=";
+    	String key = "361d7b4b2da2a9f5";
+    	String urlString = url+key
+    	+ str
+    	+ "&format=json";
+
+    	String results = "";
+
+    	//try-catchで囲む
+    	InputStream stream;
+		try {
+			stream = new URL(urlString).openStream();
+
+    		//文字列のバッファを構築
+    		StringBuffer sb = new StringBuffer();
+    		String line = "";
+    		//文字型入力ストリームを作成
+    		BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+    		//読めなくなるまでwhile文で回す
+    		while((line = br.readLine()) != null) {
+    			sb.append(line);
+    		}
+    		stream.close();
+    		String script = sb.toString();
+
+    		//ObjectMapperオブジェクトの宣言
+    		ObjectMapper mapper = new ObjectMapper();
+
+    		//JSON形式をクラスオブジェクトに変換
+    		JsonNode node = mapper.readTree(script).get("results").get("shop");
+
+    		//クラスオブジェクトの中から必要なものだけを取りだす
+    		for (int i = 0; i < node.size(); i++) {
+    			String name = node.get(i).get("name").asText();
+    			results = results + name + "\n";
+    		}
+    		return results;
+		} catch (MalformedURLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return results;
     }
 }
