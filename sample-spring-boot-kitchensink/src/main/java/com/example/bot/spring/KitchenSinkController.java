8ad28f8debc84abb7efb0e6a026cf74941c77e3c
yoshiyuki <yoshiyuki@yoshiyuki-System-Product-Name>
2022-02-01 01:00:08 +0900

    メッセージバグ修正

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -38,6 +38,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.google.common.io.ByteStreams;
+import com.connector.UrlConnecter;
 
 import com.linecorp.bot.client.LineBlobClient;
 import com.linecorp.bot.client.LineMessagingClient;
@@ -631,17 +632,16 @@ public class KitchenSinkController {
                 break;
             default:
                 log.info("Returns echo message {}: {}", replyToken, text);
-//                this.replyText(
-//                        replyToken,
-//                        text
+
                 log.info("event: " + event);
                 final String originalMessageText = text;
                 	log.info(originalMessageText);
                 String subText = shopAndKuchikomiSearch(originalMessageText);
                 
-                	
-                return new TextMessage(subText);
-//                );
+                this.replyText(
+                        replyToken,
+                        subText
+                	);
                 break;
         }
     }
